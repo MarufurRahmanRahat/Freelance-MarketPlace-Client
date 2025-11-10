cbd2d10d5f49bb1d32655a50d605276e9074e905
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-11-10 22:38:43 +0600

    created necessary routes, will add further whenever needed

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,9 +1,18 @@
+
 import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
+import { Outlet } from 'react-router';
 
 const MainLayout = () => {
     return (
-        <div>
-            this is main layout
+       <div className='flex flex-col min-h-screen'>
+            <Navbar></Navbar>
+            <div className='max-w-screen-2xl w-full flex-1'>
+                <Outlet />
+            </div>
+            <Footer></Footer>
+            {/* <Toaster /> */}
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,76 @@
 import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import MainLayout from '../Layouts/MainLayout';
+import Error from '../Pages/Error';
+import Home from '../Pages/Home';
+import Signin from '../Pages/Signin';
+import Login from '../Pages/Login';
+import Jobs from '../Pages/Jobs';
+import Update from '../Pages/Update';
+import JobDetails from '../Pages/JobDetails';
+import AddJob from '../Pages/AddJob';
+import AcceptedTask from '../Pages/AcceptedTask';
 
-const Routes = () => {
-    return (
-        <div>
-            this is routes.
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: MainLayout,
+        // errorElement: <Error></Error>,
 
-export default Routes;
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                path: "signup",
+                Component: Signin
+            },
+            {
+                path: "login",
+                Component: Login
+            },
+            {
+                path: '/jobs',
+                Component: Jobs
+            },
+            {
+                path: '/update',
+                Component: Update
+            },
+            {
+                path: '/job-details',
+                Component: JobDetails
+            },
+            {
+                path: '/add-job',
+                Component: AddJob
+            },
+            {
+                path: '/accepted-task',
+                Component: AcceptedTask
+            },
+
+
+
+
+
+
+            // {
+            //     path: '/app-details/:id',
+            //     element: <Privateroute><Appdetails /></Privateroute>
+            // },
+            // {
+            //     path: '/myprofile',
+            //     element: <Privateroute><Myprofile></Myprofile></Privateroute>,
+            // },
+
+
+
+        ]
+    },
+
+
+])
+
+export default router;
