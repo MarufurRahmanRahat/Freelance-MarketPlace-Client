caf6c3329eb6c8423accf2592dc41e681fcf57d3
MarufurRahmanRahat <marufrahat88@gmail.com>
2025-11-12 06:58:07 +0600

    added navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
-import { NavLink } from 'react-router';
+import React, { use } from 'react';
+import { Link, NavLink } from 'react-router';
+import { AuthContext } from '../Contexts/AuthContext';
+import { DiSenchatouch } from 'react-icons/di';
+import { SiFreelancermap } from 'react-icons/si';
 
 const Navbar = () => {
+
+    const { user, signOutUSer } = use(AuthContext)
+
+    const handleSignOut = () => {
+        signOutUSer()
+            .then(() => {
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
 
 const links = <>
@@ -12,9 +27,50 @@ const links = <>
     </>    
 
     return (
-       <div>
-        {links}
-       </div>
+       <div className="navbar bg-base-100 shadow-sm p-3 sm:p-8 md:px-14 lg:px-20 ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow">
+                        {links}
+                    </ul>
+                </div>
+                <div className="flex justify-center items-center">
+                    <Link to='/'><SiFreelancermap className='w-9 h-9 ' /></Link>
+                    <Link to='/' className=" hidden sm:block text-2xl font-semibold ml-1">Freelance<br/>MarketPlace</Link>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <div className="">
+                    {/* <img className='w-12 rounded-full mx-2' src={`${user && user.photoURL }`} referrerPolicy='no-referrer' alt=''></img> */}
+                    <div className="dropdown dropdown-hover mr-4 pt-2 ">
+                        <div tabIndex={0}  className="outline-none cursor-pointer w-12 rounded-full  ">
+                            <img className='w-12 rounded-full ' src={`${user && user.photoURL }`} referrerPolicy='no-referrer' alt=''></img>
+                        </div>
+                        <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box  w-32 p-2 shadow-sm z-10">
+                            <li><a className='text-red-900'>{`${user && user.displayName}`}</a></li>
+                           
+                        </ul>
+                    </div>
+                </div>
+                {
+                    user ? <a onClick={handleSignOut} className="btn text-[16px]">Log out</a>
+                        : <>
+                            <Link className='btn mr-1 text-[16px]' to='/login'>Login</Link>
+                            <Link className='btn text-[16px]' to='/signup'>SignUp</Link>
+                        </>
+                }
+            </div>
+        </div>
     );
 };
 
